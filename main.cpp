5855359323182fc67943e0ce1975f844b2c34c7c
ericoporto <ericoporto2008@gmail.com>
2020-06-21 11:19:35 -0300

    done the glfw introductory part in https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Base_code

--- main.cpp
+++ main.cpp
@@ -1,28 +1,49 @@
-#include "vulkan/vulkan.h"
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
 class HelloTriangleApplication {
+private:
+    GLFWwindow* window;
+public:
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
     void initVulkan() {
 
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
+        glfwDestroyWindow(window);
 
+        glfwTerminate();
     }
 };
 
