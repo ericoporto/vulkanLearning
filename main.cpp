e81a918d1883d66b92ce2c5c15090a588cbfde01
ericoporto <ericoporto2008@gmail.com>
2020-06-21 11:51:15 -0300

    finished the instance setup following here https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Instance

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 class HelloTriangleApplication {
 private:
     GLFWwindow* window;
+    VkInstance instance;
 public:
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
@@ -21,6 +23,68 @@ public:
     }
 
 private:
+    void printAvailableExtensions(){
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "available extensions:\n";
+
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+    }
+
+    void printRequiredExtensions(){
+        uint32_t extensionCount = 0;
+        const char** requiredExtensions = glfwGetRequiredInstanceExtensions(&extensionCount);
+
+        std::cout << "required extensions:\n";
+        for(int i=0;i<extensionCount; i++){
+            std::cout << '\t' << requiredExtensions[i] << '\n';
+        }
+    }
+
+    void createInstance() {
+        //tutorial part is here: https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Instance
+
+        // see https://www.khronos.org/registry/vulkan/specs/1.2-extensions/man/html/VkApplicationInfo.html
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        printAvailableExtensions(); // prints available extensions, we're not using most.
+        printRequiredExtensions(); // prints required extensions for current platform
+
+        //Vulkan is a platform agnostic API, which means that you need an extension to interface with the window system.
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount); //select appropriate extension per platform
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        // Pointer to struct with creation info
+        // Pointer to custom allocator callbacks, always nullptr in this tutorial
+        // Pointer to the variable that stores the handle to the new object
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance); //actually creates the instance
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+
+
+// main functions at top level
     void initWindow() {
         glfwInit();
 
@@ -31,7 +95,7 @@ private:
     }
 
     void initVulkan() {
-
+        createInstance();
     }
 
     void mainLoop() {
@@ -41,6 +105,8 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
