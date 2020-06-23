8887bcf3fbe32d4db7a63c26884a1012682234e0
ericoporto <ericoporto2008@gmail.com>
2020-06-23 11:57:15 -0300

    makes the compile.sh path independant

--- shaders/compile.sh
+++ shaders/compile.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+HERE="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 path_to_glslc=$(which glslc)
 if ! [ -x "$path_to_glslc" ] ; then
     echo "Can\'t find glslc. Have you installed it?"
     exit
 fi
 
-glslc shader.vert -o vert.spv
-glslc shader.frag -o frag.spv
+glslc "${HERE}/shader.vert" -o "${HERE}/vert.spv"
+glslc "${HERE}/shader.frag" -o "${HERE}/frag.spv"
